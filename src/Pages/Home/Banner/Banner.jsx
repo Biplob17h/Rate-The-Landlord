8d3bd1310b35d4ae5287c43b2b36488276e2e27b
Biplob Hossain <biplob17h@gmail.com>
2024-11-05 02:31:24 -0800

    update

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
-import img from "../../../assets/house.jfif";
+import img from "../../../assets/house.jpg";
 import { Link } from "react-router-dom";
 
 const Banner = () => {
